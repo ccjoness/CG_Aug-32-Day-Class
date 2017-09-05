1e9fdcf0382bf76c46155c60582c6f13f7ccc548
Chris <chris.charles.jones@gmail.com>
2017-09-05 10:01:12 -0700

    added some changes to scratch.py

--- scratch.py
+++ scratch.py
@@ -1,7 +1,11 @@
 diction = {'some_key': 'nothing'}
 main_key = '30-08-2017'
 total = 10
-# diction[main_key] = {'Date': main_key, 'Total': total, 0: 0, 1: 5}
-# diction['30-08-2018'] = {'Date': '30-08-2018', 'Total': 22, 0: 10, 1: 12}
+
 diction.update(some_key={'some': 'value'})
 print(diction)
+
+
+@login_required
+def i_like(thing):
+    return thing
